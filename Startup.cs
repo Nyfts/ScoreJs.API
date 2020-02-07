5fb78321ca457f23547aa8cb9cfc0775ad8573a7
Luan Jesus <luan.jesus@scopesystems.com.br>
2020-02-07 09:39:37 -0300

    add cor

--- Startup.cs
+++ Startup.cs
@@ -27,6 +27,7 @@ namespace SCOREJS.API
         {
             services.AddDbContext<DataContext>(x => x.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
             services.AddControllers();
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -46,6 +47,8 @@ namespace SCOREJS.API
             // app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors(x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
 
             app.UseAuthorization();
 
